6e31976f17bdce67c92dc7b5ac2690b91b33abf7
MarinhoCM <marinhoc384@gmail.com>
2024-11-20 10:26:27 -0300

    :sparkles: feat: acrescentando informação de usuário personal

--- lib/application/entities/user.entity.dart
+++ lib/application/entities/user.entity.dart
@@ -8,20 +8,21 @@ class User {
   String _contact;
   final List<Schedule> _schedules;
   bool _isAuthenticated;
+  bool _isPersonalTrainer;
 
-  User(
-    this._code,
-    this._password, {
-    String name = '',
-    String email = '',
-    String contact = '',
-    List<Schedule>? schedules,
-    bool isAuthenticated = false,
-  })  : _name = name,
+  User(this._code, this._password,
+      {String name = '',
+      String email = '',
+      String contact = '',
+      List<Schedule>? schedules,
+      bool isAuthenticated = false,
+      bool isPersonalTrainer = false})
+      : _name = name,
         _email = email,
         _contact = contact,
         _schedules = schedules ?? [],
-        _isAuthenticated = isAuthenticated;
+        _isAuthenticated = isAuthenticated,
+        _isPersonalTrainer = isPersonalTrainer;
 
   String get code => _code;
   String get password => _password;
@@ -30,6 +31,7 @@ class User {
   String get contact => _contact;
   List<Schedule> get schedules => _schedules;
   bool get isAuthenticated => _isAuthenticated;
+  bool get isPersonalTrainer => _isPersonalTrainer;
 
   set code(String value) => _code = value;
   set password(String value) => _password = value;
@@ -37,9 +39,10 @@ class User {
   set email(String value) => _email = value;
   set contact(String value) => _contact = value;
   set isAuthenticated(bool value) => _isAuthenticated = value;
+  set isPersonalTrainer(bool value) => _isPersonalTrainer = value;
 
   String text() {
-    return 'Nome: $_name, Email: $_email, Código: $_code, Senha: $_password';
+    return 'Nome: $_name, Email: $_email, Código: $_code, Senha: $_password, é personal: ${isPersonalTrainer ? "sim" : "não"}';
   }
 }
 
@@ -55,7 +58,7 @@ class Trainer extends User {
     super.schedules,
     super.isAuthenticated,
     List<User>? clients,
-  })  : _clients = clients ?? [];
+  }) : _clients = clients ?? [];
 
   List<User> get clients => _clients;
 
